9de9c21b95cdbc85ece5f79b815ab3c02fa7378e
Charles Coqueret <charles.coqueret@gmail.com>
2022-03-18 12:08:36 +0100

    Test: Atoms/Layout Extending layout testing

--- src/Components/Atoms/Layout/__tests__/Section.test.tsx
+++ src/Components/Atoms/Layout/__tests__/Section.test.tsx
@@ -25,4 +25,32 @@ describe('Section Component', () => {
 
     expect(container).toMatchSnapshot();
   });
+
+  it('Section renders not collapsable', async () => {
+    const container = render(
+      <Section collapsable={false} openInitially={true} title='TITLE' dataTestId='DATA-TEST-ID'>
+        <div id='CHILD1' />
+        <div id='CHILD2' />
+      </Section>,
+    );
+
+    expect(container).toMatchSnapshot();
+
+    const item = container.getByTestId('DATA-TEST-ID');
+
+    userEvent.click(item);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Section renders not collapsable', async () => {
+    const container = render(
+      <Section collapsable={false} openInitially={false} title='TITLE' dataTestId='DATA-TEST-ID'>
+        <div id='CHILD1' />
+        <div id='CHILD2' />
+      </Section>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
 });
