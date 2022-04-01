74a019ec26c835f93566f214e96993fa75faf2e9
Charles Coqueret <charles.coqueret@gmail.com>
2022-04-01 17:33:27 +0200

    Fix: Organisms/Form change handle for IOption with number value

--- src/Components/Organisms/Form/FormField.tsx
+++ src/Components/Organisms/Form/FormField.tsx
@@ -168,7 +168,7 @@ const FormField = <T,>(props: IFormFieldProps<T>): ReactElement => {
             options={localOptions}
             mandatory={requiredFromValidation || field.mandatory}
             name={field.dataIndex.toString()}
-            onChange={(newValue: Array<string> | null | undefined) => {
+            onChange={(newValue: Array<string | number> | null | undefined) => {
               handleChange(field.dataIndex, newValue as unknown as T[keyof T]);
             }}
             inputValue={(data && (data[field.dataIndex] as unknown as Array<string> | undefined)) || undefined}
@@ -239,7 +239,7 @@ const FormField = <T,>(props: IFormFieldProps<T>): ReactElement => {
             options={localOptions}
             mandatory={requiredFromValidation || field.mandatory}
             name={field.dataIndex.toString()}
-            onChange={(newValue: string | null | undefined) => {
+            onChange={(newValue: string | number | null | undefined) => {
               handleChange(field.dataIndex, newValue as unknown as T[keyof T]);
             }}
             inputValue={(data && (data[field.dataIndex] as unknown as string | undefined)) || undefined}
