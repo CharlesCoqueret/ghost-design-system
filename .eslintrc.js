ba26b354eb918f9c534627cae4f6bbf09f415f24
Charles Coqueret <charles.coqueret@gmail.com>
2022-04-22 19:12:04 +0200

    Build: fix switch-exhaustiveness-check by using strictNullChecks

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,7 +42,6 @@ module.exports = {
         selector: 'interface',
       },
     ],
-    '@typescript-eslint/switch-exhaustiveness-check': rules.ON,
     'react-hooks/rules-of-hooks': rules.ON,
     'react-hooks/exhaustive-deps': rules.OFF,
     'prettier/prettier': rules.ON,
