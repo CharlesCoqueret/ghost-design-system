1fcba49fee67520b92e9321ac5d9b4e1747634b9
Charles Coqueret <charles.coqueret@gmail.com>
2022-04-23 00:38:01 +0200

    Feat: Atoms/Modal Offer a way to disable tabbing outside the modal

--- src/Components/Atoms/Modal/Modal.tsx
+++ src/Components/Atoms/Modal/Modal.tsx
@@ -121,7 +121,7 @@ const Modal = (props: PropsWithChildren<IModalProps>): ReactElement => {
   if (show)
     return (
       <Portal>
-        <div className='gds-modal-overlay' role='dialog'>
+        <div className='gds-modal-overlay'>
           <div
             className={classnames('modal-content', {
               'size-sm': size === 'sm',
