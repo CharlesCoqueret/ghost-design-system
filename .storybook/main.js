c81aab3d7c8cb913f9b3235f77b5624c594e2020
Charles Coqueret <charles.coqueret@gmail.com>
2022-04-28 09:25:06 +0200

    Build: use webpack5 for Storybook Build: extends Storybook docs with mdx
    Docs: Organisms/ActionBar add mdx documentation
    Docs: Organisms/Filter add mdx documentation
    
    Fix: Organisms/ActionBar allow use of icon in action button

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,18 +3,30 @@ const path = require('path');
 const maxAssetSize = 250 * 1024;
 
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-a11y',
     '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
     '@storybook/addon-links',
     '@storybook/addon-postcss',
     '@storybook/addon-storysource',
   ],
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  framework: '@storybook/react',
   webpackFinal: async (config) => {
+    config.module.rules.unshift({
+      test: /\.scss$/i,
+      resourceQuery: /raw/,
+      type: 'asset/source',
+    });
+
     // SCSS ALL EXCEPT local
     config.module.rules.push({
       test: /\.module\.scss$/i,
+      resourceQuery: { not: [/raw/] },
       use: ['style-loader', 'css-loader', 'sass-loader'],
     });
 
@@ -22,6 +34,7 @@ module.exports = {
     config.module.rules.push({
       test: /\.scss$/i,
       exclude: /\.module\.scss$/i,
+      resourceQuery: { not: [/raw/] },
       use: ['style-loader', 'css-loader', 'sass-loader'],
       include: path.resolve(__dirname, '../'),
     });
