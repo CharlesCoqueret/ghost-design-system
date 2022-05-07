cb97eb4c621aa02430ba5bc07f59baed8eac531f
Charles Coqueret <1748662+CharlesCoqueret@users.noreply.github.com>
2022-05-07 19:37:19 +0200

    Delete src/Components/Organisms/Sidebar directory

--- src/Components/Organisms/Sidebar/index.ts
+++
@@ -1,5 +0,0 @@
-export type { ISideBarItemProps } from './SideBarItem';
-export type { ISideBarSectionProps } from './SideBarSection';
-export { default as SideBar } from './SideBar';
-export { default as SideBarItem } from './SideBarItem';
-export { default as SideBarSection } from './SideBarSection';
