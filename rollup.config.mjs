876c9270e653c24355e937899ac4a9368c08c2bb
Charles Coqueret <cct@tinubu.com>
2022-11-13 17:00:58 +0100

    Build bump various dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,7 +5,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import scss from 'rollup-plugin-scss';
 
-import packageJson from './package.json';
+import packageJson from './package.json' assert { type: 'json' };
 
 export default {
   input: 'src/index.ts',
